0cae09105c8870706f473bb333b5efd02982ebf2
Curran <curran.kelleher@gmail.com>
2015-05-15 22:05:08 -0700

    Remove traceur completely

--- config.js
+++ config.js
@@ -9,9 +9,7 @@ System.config({
 
 System.config({
   "map": {
-    "simplePlugin": "github:curran/simplePlugin@master",
-    "traceur": "github:jmcriffey/bower-traceur@0.0.88",
-    "traceur-runtime": "github:jmcriffey/bower-traceur-runtime@0.0.88"
+    "simplePlugin": "github:curran/simplePlugin@master"
   }
 });
 
