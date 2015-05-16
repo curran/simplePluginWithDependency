a8dd916115d0b9cbd4ef84833658e89e12fa5fd0
Curran <curran.kelleher@gmail.com>
2015-05-15 21:54:05 -0700

    First commit

---
+++ config.js
@@ -0,0 +1,17 @@
+System.config({
+  "baseURL": "/",
+  "transpiler": "traceur",
+  "paths": {
+    "*": "*.js",
+    "github:*": "jspm_packages/github/*.js"
+  }
+});
+
+System.config({
+  "map": {
+    "simplePlugin": "github:curran/simplePlugin@master",
+    "traceur": "github:jmcriffey/bower-traceur@0.0.88",
+    "traceur-runtime": "github:jmcriffey/bower-traceur-runtime@0.0.88"
+  }
+});
+
---
+++ src/simplePluginWithDependency.js
@@ -0,0 +1,3 @@
+define(["simplePlugin"], function (simplePlugin){
+  return "I am a plugin that depends on '" + simplePlugin + "'";
+});
